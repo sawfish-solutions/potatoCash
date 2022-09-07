833a1e84d76c2c2b114d199a894141b19979b779
jkl <jkl@bitbung.net>
2022-09-07 15:47:17 +1000

    Added addexpense view, delete view

--- potatoCash/expenses/views.py
+++ potatoCash/expenses/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 from .models import Expenses
 
 def index(request):
     allExpenses = Expenses.objects.all().values()
-    template = loader.get_template('index-bulma.html')
+    template = loader.get_template('index.html')
     context = {
         'allExpenses': allExpenses,
     }
@@ -14,3 +15,18 @@ def index(request):
 def add(request):
     template = loader.get_template('add.html')
     return HttpResponse(template.render({}, request))
+
+def addexpense(request):
+    a = request.POST['user_name']
+    b = request.POST['first_name']
+    c = request.POST['last_name']
+    d = request.POST['exp_category']
+    e = request.POST['exp_value']
+    expense = Expenses(username=a, firstname=b, lastname=c, expcategory=d, expvalue=e)
+    expense.save()
+    return HttpResponseRedirect(reverse('index'))
+
+def delete(request, id):
+    expense = Expenses.objects.get(id=id)
+    expense.delete()
+    return HttpResponseRedirect(reverse('index'))
