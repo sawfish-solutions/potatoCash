b666b58021725ab0c6cbaef062ffc650f19bdd3a
jkl <jkl@bitbung.net>
2022-09-07 14:24:37 +1000

    Added expense view

--- potatoCash/expenses/views.py
+++ potatoCash/expenses/views.py
@@ -1,7 +1,16 @@
-from http.client import HTTPResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Expenses
 
 def index(request):
-    return HttpResponse("Hello, world. You are at the expenses index.")
-# Create your views here.
+    allExpenses = Expenses.objects.all().values()
+    template = loader.get_template('index-bulma.html')
+    context = {
+        'allExpenses': allExpenses,
+    }
+    return HttpResponse(template.render(context, request))
+
+def add(request):
+    template = loader.get_template('add.html')
+    return HttpResponse(template.render({}, request))
