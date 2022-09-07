e3104bfabab748689deab2e8a13aa4c32c216d17
jkl <jkl@bitbung.net>
2022-09-07 14:22:33 +1000

    Added "Expense" model

--- potatoCash/expenses/models.py
+++ potatoCash/expenses/models.py
@@ -1,3 +1,10 @@
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+class Expenses(models.Model):
+    user_name = models.CharField(max_length=255),
+    first_name = models.CharField(max_length=255),
+    last_name = models.CharField(max_length=255),
+    exp_category = models.CharField(max_length=255),
+    exp_value = models.FloatField(max_length=100)
