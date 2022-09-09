ea39567d34972eab833cbfc209c6b989cfa4c67e
jkl <jkl@bitbung.net>
2022-09-09 11:54:54 +1000

    Updated users views.py. Using new template.

--- potatoCash/users/views.py
+++ potatoCash/users/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 
-#def index(request):
-#    template = loader.get_template('index.html')
-#    return HttpResponse(template.render({}, request))
 def index(request):
-    template = loader.get_template('index.html')
+    template = loader.get_template('user_index.html')
     return HttpResponse(template.render({}, request))
