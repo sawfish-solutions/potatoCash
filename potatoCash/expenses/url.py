1c4f33951b8b993daae25a4e58f6acb724792408
jkl <jkl@bitbung.net>
2022-09-09 11:52:46 +1000

    renamed url name

--- potatoCash/expenses/url.py
+++ potatoCash/expenses/url.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='exp_index'),
     path('add/', views.add, name='add'),
     path('add/addexpense/', views.addexpense, name='addexpense'),
     path('delete/<int:id>', views.delete, name='delete')
